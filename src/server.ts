6129d77cecec2ceb1d535deb3c26fb7618570bee
Amayuru1999 <amarasingheau@gmail.com>
2024-08-13 15:50:47 +0530

    chore: Add dot and env dependencies, update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,42 @@
-import express, {Express} from 'express'
-import itemRoutes from "./routes/item-routes";
-import {connectToMongoDB} from "./configs/mongodb";
-import dotenv from 'dotenv'
-import path from "path";
-import {Server} from "http";
-dotenv.config()
+import express, { Express } from 'express';
+import itemRoutes from './routes/item-routes';
+import { connectToMongoDB } from './configs/mongodb';
+import dotenv from 'dotenv';
+import path from 'path';
+import { Server } from 'http';
 
-const PORT = 5000
-const app = express()
-let server: Server
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+let server: Server;
 
 // json serialize
-app.use(express.json())
+app.use(express.json());
 
 // Serve static files from the "public" directory
 // app.use(express.static(path.join(__dirname, 'public')));
 
-
 // routes
-app.get('/', (req, res)=> {
-    res.status(200).json({message: 'Hello Metaroon 2024!'})
-    // res.sendFile(path.join(__dirname, 'public/index.html'));
-})
-app.use('/api/v1/items', itemRoutes)
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello Metaroon 2024!' });
+  // res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.use('/api/v1/items', itemRoutes);
 
 // Start the express app
-connectToMongoDB('mongodb+srv://stoXmod:5VJbnUadD3lLZPJu@cluster0.avfm1yl.mongodb.net/test?retryWrites=true&w=majority').then(()=> {
-    console.log('✅ Mongodb Connected!')
-    server = app.listen(PORT, ()=> {
-        console.log(`🚀 Server is running on port ${PORT}`)
-    })
-}).catch((ex)=> {
-    console.log('🔴 Connection failed with MongoDB!', ex)
-})
+const mongoUri = process.env.MONGO_URL|| '';
 
-export {app,server}
+connectToMongoDB(mongoUri)
+  .then(() => {
+    console.log('✅ Mongodb Connected!');
+    server = app.listen(PORT, () => {
+      console.log(`🚀 Server is running on port ${PORT}`);
+    });
+  })
+  .catch((ex) => {
+    console.log('🔴 Connection failed with MongoDB!', ex);
+  });
 
+export { app, server };
